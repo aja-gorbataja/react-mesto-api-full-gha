022b138963e94c7e6066cfe576ed75cb290507c2
aja_gorbataja <aja.gorbataja@yandex.ru>
2023-05-31 23:58:19 +0300

    fix login error 17

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,16 +3,26 @@ import PopupWithForm from "./PopupWithForm";
 import Header from "./Header";
 
 function Login({ handleLogIn }) {
-  const [ formValue, setFormValue ] = useState({email: '', password: ''});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function handleChange(evt) {
+  function handleEmailInput(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handlePasswordInput(evt) {
+    setPassword(evt.target.value);
+  }
+  //const [ formValue, setFormValue ] = useState({email: '', password: ''});
+
+  /*function handleChange(evt) {
     const { name, value } = evt.target;
     setFormValue({ ...formValue, [name]: value})
-  }
+  }*/
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    handleLogIn(formValue)
+    handleLogIn(email, password)
   }
 
   return (
@@ -20,9 +30,9 @@ function Login({ handleLogIn }) {
     <Header title="Регистрация" navLink='/sign-up' />
     <PopupWithForm name="login" title="Вход" button="Войти" onSubmit={handleSubmit}  >
         <fieldset className="form__input">
-        <input id="email" type="email" name="email" value={formValue.email} className="form__input-email popup__input" placeholder="Email" onChange={handleChange}  />
+        <input id="email" type="email" name="email" value={email} className="form__input-email popup__input" placeholder="Email" onChange={handleEmailInput}  />
           <span className="username-error popup__input-error"></span>
-          <input id="password" name="password" type="password" value={formValue.password} className="form__input-password popup__input" placeholder="Пароль" onChange={handleChange} />
+          <input id="password" name="password" type="password" value={password} className="form__input-password popup__input" placeholder="Пароль" onChange={handlePasswordInput} />
           <span className="description-error popup__input-error"></span>
         </fieldset>
       </PopupWithForm>
